896b59589f85e85e5edcba91a5349a5df95e985a
Clem <imie@localhost.localdomain>
2016-12-21 09:24:50 +0100

    initial commit

---
+++ app/templates/home.php
@@ -0,0 +1,10 @@
+<h1>Accueil !</h1>
+
+<p>Ne pas oublier de modifier le fichier <code>app/config/config.php</code> !</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+<!-- // ### Accueil
+// - La page d'accueil affiche une liste de films, trié du meilleur au moins bon. La liste est paginée.
+// - 2 formulaires au haut de la page permettent de rechercher par mot-clef, 
+// par genre de film ou de filtrer par date
+// - Seuls les affiches de films sont présentées. Elles sont cliquables et mènent vers la page de détails. -->
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+	//ceci est le contrôleur frontal
+	//il doit être short & sweet
+
+	//charge nos définitions de routes
+	require("app/config/routes.php");
+
+	//configuration de l'appli
+	require("app/config/config.php");
+
+	//charge toutes nos dépendances composer
+	if (file_exists("vendor/autoload.php")){
+		require("vendor/autoload.php");
+	}
+
+	//auto chargement de nos classes
+	spl_autoload_register(function($className){
+		$path = "app" . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $className) . ".php";
+		if (file_exists($path)){
+			require($path);
+		}
+	});
+
+	//le dispatcher trouve la correspondance entre nos routes et l'url
+	$p = (empty($_GET['p'])) ? "/" : $_GET['p'];
+	$dispatcher = new Controller\Dispatcher();
+	$dispatcher->dispatch($routes, $p);
+
+// L'internaute peut 
+// 	- consulter une liste de films, tous de bonne qualité
+// 	- rechercher (dans un premier temps) par mot-clef, par genre ou par date
+// 	- donner une note à un film
+// 	- créer un compte, se connecter/déconnecter, oublier son mot de passe
+// 	- ajouter/retirer des films à sa liste personnelle de films à voir (watchlist)
+// 	- partager une fiche de film, ou sa watchlist
+
+// Le client veut de son côté pouvoir : 
+// 	- ajouter/modifier/supprimer des films de la base de données. Il a besoin d'une interface web.
+
+// ### Détails d'un film
+// - Toutes les infos sur le film sont affichées
+// - Un widget permettant de donner une note sur 10 (ie. style étoiles) (connectés seulement)
+// - Un bouton permet d'ajouter/supprimer de sa watchlist (connectés seulement)
+// - Un formulaire permet de partager par email le film (connectés seulement)
