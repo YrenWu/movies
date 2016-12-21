d217d2d75a26aeee66868e320f0614975673ac9d
Clem <imie@localhost.localdomain>
2016-12-21 11:00:56 +0100

    show pictures in home page

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,7 @@
 	//url racine du site. Sert à générer des URLs absolues
 	const BASE_URL 		= "http://localhost/TP-Movies/movies/"; 			//adresse complète menant à index.php. Modifier CHEMIN ! Utile pour tous les liens dans le HTML.
 	const UPLOAD_DIR 	= __DIR__ . "/../../public/uploads/";	//chemin menant au dossier d'upload
+	const PICS_DIR 		= BASE_URL . "public/pics/";
 
 	//infos de connexion à la db
 	const DB_HOST = "localhost";
--- app/templates/home.php
+++ app/templates/home.php
@@ -4,15 +4,13 @@
 
 	<?php foreach ($movies as $movie) { ?> 
 		<article>
-			<h5><?= $movie->getTitle() ?></h5>
-			<?= $movie->getDirectors() ?><br>
-			<?= $movie->getYear() ?> <?= $movie->getRating() ?>
+			<a href=""><img class="thumbnail" src="<?= PICS_DIR . $movie->getImdbId() . ".jpg" ?>" alt="<?= $movie->getTitle() ?>"></a>
 
 		</article>
 	<?php } ?>
 
 <!-- // ### Accueil
-// - La page d'accueil affiche une liste de films, trié du meilleur au moins bon. La liste est paginée.
+// La liste est paginée.
 // - 2 formulaires au haut de la page permettent de rechercher par mot-clef, 
 // par genre de film ou de filtrer par date
 // - Seuls les affiches de films sont présentées. Elles sont cliquables et mènent vers la page de détails. -->
