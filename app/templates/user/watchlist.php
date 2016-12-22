382e3655b24cc05de7263b7f05c96ba7eb3ec777
Clem <imie@localhost.localdomain>
2016-12-22 16:40:53 +0100

    add pics in watchlist

--- app/templates/user/watchlist.php
+++ app/templates/user/watchlist.php
@@ -1,10 +1,18 @@
 <?php
+
+	$movieManager = new Model\Manager\MovieManager;
+
 	if(!empty($_SESSION['user'])) { 
 	$user = $_SESSION['user']; ?>
 
 	<h1>Welcome <?= $user->getName() ?></h1>
-	<?= var_dump($user) ?>
-
+	<?php 
+		$watchlist = unserialize($user->getWatchlist());
+		foreach ($watchlist as $movieId) {
+			$movie = $movieManager->findOne($movieId);
+			echo($movie->getTitle());
+			echo '<img class="thumbnail" src="'. PICS_DIR . $movie->getImdbId() . '.jpg">';
+		}?>
 
 <?php	}
 ?> <!-- vérifier que l'utilisateur est loggué pour afficher et afficher ses favoris -->
